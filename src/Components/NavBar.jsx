001f05fd6f877433c498155f06ac38f96ad33d1e
eshogheme <sabokingsley4@gmail.com>
2025-05-08 19:28:42 -1100

    desktop and tab view done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,21 +8,33 @@ export function NavBar() {
   return (
     <>
     <nav className="bg-white flex justify-between">
-        <img className="w-[20%] m-2" 
+        <img className="w-[10%] m-2 md:w-[7%] lg:w-[5%] xl:w-[5%] " 
             src={logo} 
             alt="" 
         />
-        <div className="p-2 flex justify-between mr-5 gap-6">
-            <button 
-            className="" 
-            onClick={()=>{
-                setActiveSidebar("search");
-            }}
-            >
-                <FaSearch size={23} />
-            </button>
-                
+       <div className="p-2 flex justify-between mr-5 gap-6 lg:p-0">
+            <div className="relative flex lg:block w-full lg:w-[70%] lg:mt-7lg:flex lg:mt-3 xl:mt-6">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="hidden lg:block lg:w-full lg:border lg:rounded-lg lg:py-2 lg:pl-12 lg:pr-4 lg:text-gray-800 lg:placeholder:text-lg lg:placeholder:text-gray-400 lg:placeholder:pl-2 2xl:mt-6"
+                />
+                <button 
+                    className="lg:absolute lg:left-3 lg:top-[30%] lg:-translate-y-1/2 lg:text-black "
+
+                onClick={()=>{
+                    setActiveSidebar("search");
+                }}
+                >
+                <FaSearch
+                    className="text-black"
+                    size={20}
+                />
+                </button>
+        </div>
+
             <button
+            className='lg:hidden'
              onClick={()=>{
                 setActiveSidebar("user");
             }}
@@ -30,6 +42,12 @@ export function NavBar() {
                 <FaUser size={23} />
             </button>
 
+            
+            <button 
+                className="hidden lg:inline-block lg:transition-shadow duration-200"
+            > 
+                <FaHeart size={23} />
+            </button>
             <button 
             
             > 
@@ -37,6 +55,7 @@ export function NavBar() {
             </button>
 
             <button 
+                className='lg:hidden'
                 onClick={()=>{
                     setActiveSidebar("bars");
                 }}
